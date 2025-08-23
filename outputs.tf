17a84d0e4e202d6ddbc7ab3ced004a6d2dde880e
Leandro Minatti de Souza <minatti.leandro@gmail.com>
2025-08-23 17:29:19 -0300

    remove cluster_name

--- outputs.tf
+++ outputs.tf
@@ -5,7 +5,3 @@ output "cluster_endpoint" {
 output "cluster_ca_certificate" {
   value = module.eks.cluster_certificate_authority_data
 }
-
-output "cluster_name" {
-  value = module.eks.cluster_name
-}
