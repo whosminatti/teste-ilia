5de1757319f048decdd6751e6b42afac653d8311
Leandro Minatti de Souza <minatti.leandro@gmail.com>
2025-08-23 11:34:00 -0300

    Comenta provider k8s

--- provider.tf
+++ provider.tf
@@ -20,16 +20,16 @@ provider "aws" {
   region = var.region
 }
 
-data "aws_eks_cluster" "cluster" {
-  name = var.project_name
-}
+# data "aws_eks_cluster" "cluster" {
+#   name = var.project_name
+# }
 
-data "aws_eks_cluster_auth" "cluster" {
-  name = data.aws_eks_cluster.cluster.name
-}
+# data "aws_eks_cluster_auth" "cluster" {
+#   name = data.aws_eks_cluster.cluster.name
+# }
 
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-}
+# provider "kubernetes" {
+#   host                   = data.aws_eks_cluster.cluster.endpoint
+#   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+#   token                  = data.aws_eks_cluster_auth.cluster.token
+# }
