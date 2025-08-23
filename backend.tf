cacda06803444ace675a330c2f823a466ce3d77e
Leandro Minatti de Souza <minatti.leandro@gmail.com>
2025-08-23 01:31:34 -0300

    Ajusta backend

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "teste-ilia-deploy-k8s"
-    key            = "teste-ilia.tfstate"
+    key            = "terraform-teste-ilia.tfstate"
     region         = "us-east-1"
     encrypt        = true
   }
