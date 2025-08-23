7bb499e32ec00156eb066695e9fd9a0c55a4cba0
Leandro Minatti de Souza <minatti.leandro@gmail.com>
2025-08-23 18:14:24 -0300

    Add CSI Driver Add-on

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -41,3 +41,20 @@ resource "aws_eks_node_group" "this" {
   # Garante que o cluster esteja pronto antes de criar o node group
   depends_on = [aws_eks_cluster.this]
 }
+
+# EBS CSI Driver Add-on
+resource "aws_eks_addon" "ebs_csi_driver" {
+  cluster_name             = aws_eks_cluster.this.name
+  addon_name               = "aws-ebs-csi-driver"
+  addon_version            = "v1.35.0-eksbuild.1"
+  resolve_conflicts        = "OVERWRITE"
+  service_account_role_arn = var.ebs_csi_driver_role_arn
+
+  depends_on = [
+    aws_eks_node_group.this
+  ]
+
+  tags = {
+    Name = "${var.project_name}-ebs-csi-driver"
+  }
+}
